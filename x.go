2cd0066138e871c0c46f3e3c1e6a29ed46b99e46
Alexey Ermakov <n3r0bi0m4n@gmail.com>
2020-12-11 01:26:45 +0300

    update module name and Hi function

--- x.go
+++ x.go
@@ -1,8 +1,8 @@
-package testgomodule
+package testgomod
 
 import "fmt"
 
 // Hi prints greetings for someone
 func Hi(name string) {
-	fmt.Printf("Hi, %s!", name)
+	fmt.Printf("Hi, %s!\n", name)
 }
